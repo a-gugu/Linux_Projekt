3395f7e0e537d53a4db7d3dfe9e6964400696ae6
Alexander Gugu <alexandergugu@Alexander-Gugus-MacBook-Pro.local>
2013-05-27 14:44:47 +0200

    works with params

--- src/ircbot.cpp
+++ src/ircbot.cpp
@@ -15,44 +15,44 @@
 #include <unistd.h>
 #endif
 
-using namespace std;
-
-const unsigned int MAX_LINE = 1024; //Größe des Empfangpuffers
-const int PORT = 6665;
-const char *HOST = "irc.psinet.com";
-
 #ifdef WIN32
-SOCKET sockfd;        
+SOCKET sockfd;
 #else
 int sockfd;
 #endif
+
+using namespace std;
+
+const unsigned int MAX_LINE = 1024;
+
 void irc_connect();
 void irc_disconnect();
-void s2u(const char *msg);
-void ping_parse(const string &buffer);
-void irc_parse(string buffer);
 void irc_identify();
+void irc_parse(string buffer);
+
+void s2u(const char* msg);
+void ping_parse(const string &buffer);
 void bot_functions(const string &buffer);
 
-void irc_connect(){
+
+void irc_connect(int port, char* host){
 	
 #ifdef WIN32
-	//Windows-Socket initialisieren
 	WSADATA wsa;
-	if( WSAStartup( MAKEWORD( 2, 0 ), &wsa ) != 0)
+	if(WSAStarup(MACKEWORD(2,0),&wsa) != 0)
 		exit(1);
 #endif
 	
-	sockfd = socket( AF_INET, SOCK_STREAM, 0 );
+	const int PORT = port;
+	const char *HOST = host;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	
-	//Cast nur in Windows nötig
-	if (static_cast<int> (sockfd) < 0) {
+	if(static_cast<int> (sockfd) < 0){
 		perror("socket()");
 		irc_disconnect();
 		exit(1);
 	}
 	
-	//Adressermittlung
 	hostent *hp = gethostbyname(HOST);
 	if(!hp){
 		cerr << "gethostbyname()" << endl;
@@ -61,105 +61,113 @@ void irc_connect(){
 	}
 	
 	sockaddr_in sin;
-	memset( (char*)&sin, 0, sizeof(sin) );
+	memset((char*)&sin, 0, sizeof(sin));
 	sin.sin_family = AF_INET;
-	memcpy( (char*)&sin.sin_addr, hp->h_addr, hp->h_length );
-	sin.sin_port = htons(PORT);
+	memcpy((char*)&sin.sin_addr, hp->h_addr, hp->h_length);
+	sin.sin_port =htons(PORT);
 	memset(&(sin.sin_zero), 0, 8*sizeof(char));
 	
-	//Verbindungsaufbau
-	if (connect(sockfd, (sockaddr*)&sin, sizeof(sin)) == -1) {
-		perror("connect()");
+	if(connect(sockfd, (sockaddr*)&sin, sizeof(sin)) == -1){
+		perror("connenct()");
 		irc_disconnect();
 		exit(1);
 	}
 }
 
-void irc_disconnect() {
+void irc_disconnect(){
+	
 #ifdef WIN32
-	//Windows-Socket freigeben
 	closesocket(sockfd);
 	WSACleanup();
 #else
 	close(sockfd);
 #endif
+	
 }
 
-void s2u(const char *msg){ //send to uplink
+void s2u(const char* msg){
 	send(sockfd, msg, strlen(msg), 0);
 }
 
+void irc_identify(){
+	
+	s2u("NICK Frosch78\r\n");
+	
+	s2u("USER Frosch78 0 0 :Frosch78\r\n");
+	
+	s2u("PRIVMSG NickServ IDENTIFY password\r\n");
+	
+	s2u("JOIN #channel\r\n");
+	
+	s2u("PRIVMSG #channel :Froschchannel\r\n");
+}
+
 void ping_parse(const string &buffer){
+	
 	size_t pingPos = buffer.find("PING");
 	
-	if (pingPos != string::npos) {
+	if(pingPos != string::npos){
+		
 		string pong("PONG" + buffer.substr(pingPos + 4) + "\r\n");
 		cout << pong;
+		
 		s2u(pong.c_str());
+	}
+	
+}
+
+void bot_functions(const string &buffer){
+	
+	size_t pos = 0;
+	
+	if ((pos = buffer.find(":say ")) != string::npos) {
+		s2u(("PRIVMSG #channel:" + buffer.substr(pos + 5) + "\r\n").c_str());
+	}
+	else if(buffer.find(":User!User@User.user.insiderZ.DE") == 0 && buffer.find("exit") != string::npos){
+		s2u("PRIVMSG #channel :Cya\r\n");
+		irc_disconnect();
+		exit(0);
 	}
 }
 
 void irc_parse(string buffer){
 	
-	if (buffer.find("\r\n") == (buffer.length() - 2)) {
-		buffer.erase(buffer.length() - 2);
-		ping_parse(buffer);
-		bot_functions(buffer);
-	}
-}
-
-void irc_identify(){
+	if(buffer.find("\r\n") == buffer.length() -2)
+		buffer.erase(buffer.length() -2);
 	
-	s2u("NICK Bot\r\n");							//Nickname
-	s2u("USER Bot 0 0 :VellasBot\r\n");				//Userdaten
-	s2u("PRIVMSG NickServ IDENTIFY password\r\n");	//Identifizierung
-	s2u("JOIN #channel\r\n");						//Channel betreten
-	s2u("PRIVMSG #channel :Hello!\r\n");			//Begrüßungsnachricht
-}
-
-void bot_functions(const string &buffer) {
+	ping_parse(buffer);
 	
-	/*if (buffer.find("Hallo!") != string::npos) {
-	 s2u(("PRIVMSG #channel :Hallo!\r\n").c_str());
-	 }*/
-	
-	size_t pos = 0;
-	
-	if ((pos = buffer.find(":say ")) != string::npos) {
-		s2u(("PRIVMSG #channel :" + buffer.substr(pos + 5) + "\r\n").c_str());
-	}
-	
-	if (buffer.find(":User!User@User.user.insiderZ.DE") == 0 && buffer.find("exit") != string::npos) {
-		s2u("PRIVMSG #channel :Cya\r\n");
-		irc_disconnect();
-		exit(0);
-	}
+	bot_functions(buffer);
 	
 }
 
-int main() {
-	irc_connect();
+int main(int argc, char *argv[]){
+	
+	if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+	
+	
+	irc_connect(atoi(argv[1]),argv[2]);//"irc.europa-irc.de"
 	
 	irc_identify();
 	
 	for (;;) {
-		char buffer[MAX_LINE+1] = {0};
+		char buffer[MAX_LINE +1] = {0};
 		
-		if (recv(sockfd, buffer, MAX_LINE*sizeof(char), 0) < 0) {
-			perror("recv()");
+		if(recv(sockfd, buffer, MAX_LINE * sizeof(char), 0) < 0) {
+			perror("recv");
 			irc_disconnect();
 			exit(1);
 		}
 		
 		cout << buffer;
+		
 		irc_parse(buffer);
 	}
 	
 	irc_disconnect();
 	
+	return 0;
 }
-
-
-
-
-
